83d96a3bcb9497c9d1120b526cb17a1af0b71ea2
Gustavo <gustavogmfarias@gmail.com>
2023-03-29 16:03:17 -0300

    .

--- App.tsx
+++ App.tsx
@@ -1,4 +1,4 @@
-import { StatusBar } from "react-native";
+import { StatusBar, Platform } from "react-native";
 import OneSignal from "react-native-onesignal";
 import { NativeBaseProvider } from "native-base";
 import {
@@ -14,7 +14,17 @@ import { Loading } from "./src/components/Loading";
 
 import { CartContextProvider } from "./src/contexts/CartContext";
 
-OneSignal.setAppId("7c7c600c-6d04-4226-9178-8726cc6003af");
+const oneSignalAppId =
+  Platform.OS === "ios"
+    ? "2b542c58-e2e9-48f1-8bff-004caefe40c4"
+    : "dd6753a9-19ad-4f0f-b1c3-700cff7f9dcc";
+OneSignal.setAppId(oneSignalAppId);
+
+OneSignal.setEmail("rodrigo@email.com");
+
+OneSignal.promptForPushNotificationsWithUserResponse((response) => {
+  console.log(response);
+});
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
