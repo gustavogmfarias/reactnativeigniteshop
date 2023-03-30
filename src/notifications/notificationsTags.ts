011ae68774be2c7599c3cd5c712b6bcb35e2c5ca
Gustavo <gustavogmfarias@gmail.com>
2023-03-30 10:54:16 -0300

    .

---
+++ src/notifications/notificationsTags.ts
@@ -0,0 +1,8 @@
+import OneSignal from "react-native-onesignal";
+
+export function tagUserInfoCreate() {
+  OneSignal.sendTags({
+    user_name: "Rennan",
+    user_email: "rennan.douglas@rocketseat.team",
+  });
+}
