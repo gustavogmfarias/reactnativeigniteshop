2eb71311988a27bc514a40ca82825013826f6725
Gustavo <gustavogmfarias@gmail.com>
2023-03-31 15:17:39 -0300

    .

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,13 +1,26 @@
 import { useEffect, useState } from "react";
+import { useTheme } from "native-base";
+import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
 import OneSignal, {
   NotificationReceivedEvent,
   OSNotification,
 } from "react-native-onesignal";
-import { useTheme } from "native-base";
-import { DefaultTheme, NavigationContainer } from "@react-navigation/native";
-
 import { AppRoutes } from "./app.routes";
 import { Notification } from "../components/Notification";
+
+const linking = {
+  prefixes: ["igniteshoesapp://", "com.rocketseat.igniteshoes://"],
+  config: {
+    screens: {
+      details: {
+        path: "details/:productId",
+        parse: {
+          productId: (productId: string) => productId,
+        },
+      },
+    },
+  },
+};
 
 export function Routes() {
   const [notification, setNotification] = useState<OSNotification>();
@@ -30,7 +43,7 @@ export function Routes() {
   }, []);
 
   return (
-    <NavigationContainer theme={theme}>
+    <NavigationContainer theme={theme} linking={linking}>
       <AppRoutes />
       {notification?.title && (
         <Notification
